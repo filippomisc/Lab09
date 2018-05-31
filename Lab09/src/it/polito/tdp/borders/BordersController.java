a981598b1220489bc4f4b10175d622b13703125e
mastr <mastr@172.22.15.15>
2018-05-31 22:50:13 +0200

    v.1.6 (precedente v.1.5)
    @iniziata la seconda parte
    
    @TODO combobox

--- Lab09/src/it/polito/tdp/borders/BordersController.java
+++ Lab09/src/it/polito/tdp/borders/BordersController.java
@@ -5,17 +5,22 @@
 package it.polito.tdp.borders;
 
 import java.net.URL;
+import java.util.Collections;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import it.polito.tdp.borders.model.Country;
 import it.polito.tdp.borders.model.Model;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class BordersController {
 
 	Model m;
+	List<Country> country;
 
 	@FXML // ResourceBundle that was given to the FXMLLoader
 	private ResourceBundle resources;
@@ -23,12 +28,24 @@ public class BordersController {
 	@FXML // URL location of the FXML file that was given to the FXMLLoader
 	private URL location;
 
+	@FXML
+	private ComboBox<Country> boxPaesi;
+	
 	@FXML // fx:id="txtAnno"
 	private TextField txtAnno; // Value injected by FXMLLoader
 
 	@FXML // fx:id="txtResult"
 	private TextArea txtResult; // Value injected by FXMLLoader
 
+	private void setComboPaesi(){
+		
+		country = m.getCountries();
+		
+		Collections.sort(country);
+		
+		this.boxPaesi.getItems().addAll(country);
+		
+	}
 	@FXML
 	void doCalcolaConfini(ActionEvent event) {
 
@@ -55,6 +72,8 @@ public class BordersController {
 		txtResult.appendText("\n");
 
 		txtResult.appendText(m.getVicini());
+		
+		this.setComboPaesi();
 		}
 		
 	}
